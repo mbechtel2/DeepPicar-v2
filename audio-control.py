4879d456d66553b75c599c4d9788ffb6d0f0380f
mbechtel2 <mbechtel@ku.edu>
2019-01-25 10:32:32 -0600

    Remove unused function from audio task

--- audio-control.py
+++ audio-control.py
@@ -30,17 +30,6 @@ def read_from_wav():
     wavefile.close()
     return data
 
-def convert_to_wav(arr, rate):
-    byte_count = (len(arr))  # 32-bit floats
-    wav_file = ""
-    # write the header
-    wav_file += struct.pack('<4sL4s4sLHHLLHH4sL',
-        'RIFF', byte_count + 36,  # header size
-        'WAVE', 'fmt ',
-        16, 1, 1, rate, rate * 2, 2, 16, 'data', byte_count)  # bits / sample
-    wav_file+=b''.join(arr)
-    return wav_file
-
 inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, device="sysdefault:CARD=CameraB409241")
 inp.setchannels(1)
 inp.setrate(16000)
@@ -80,9 +69,7 @@ while True:
         for i in range(200000):
             temp, data = inp.read()
             arr.append(data)
-        #wav_data = convert_to_wav(arr, framerate)
         write_to_wav(arr)
-
         input_data = read_from_wav()
 
         start = time.time()
